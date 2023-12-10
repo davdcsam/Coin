2a9abe85c95a907a473f71600abc97746fca7f85
davdcsam <edavidcamposl@gmail.com>
2023-12-10 15:27:10 -0600

    Use lambda for declarate 'e' when calling on_click event

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 import flet as ft
 
-def btn_click(page, greetings, first_name_arg, last_name_arg):
-    def handler(e):
-        greetings.current.controls.append(
-            ft.Text(f"Hello, {first_name_arg.current.value} {last_name_arg.current.value}!")
-        )
-        first_name_arg.current.value = ""
-        last_name_arg.current.value = ""
-        page.update()
-        first_name_arg.current.focus()
-    return handler
+
+def btn_click(page, greetings, first_name_arg, last_name_arg, e):
+    greetings.current.controls.append(
+        ft.Text(f"Hello, {first_name_arg.current.value} {last_name_arg.current.value}!")
+    )
+    first_name_arg.current.value = ""
+    last_name_arg.current.value = ""
+    page.update()
+    first_name_arg.current.focus()
 
 
 def main(page):
@@ -20,7 +19,10 @@ def main(page):
     page.add(
         ft.TextField(ref=first_name, label="First name", autofocus=True),
         ft.TextField(ref=last_name, label="Last name"),
-        ft.ElevatedButton("Say hello!", on_click=btn_click(page, greetings, first_name, last_name)),
+        ft.ElevatedButton(
+            "Say hello!",
+            on_click=lambda e: btn_click(page, greetings, first_name, last_name, e),
+        ),
         ft.Column(ref=greetings),
     )
 
