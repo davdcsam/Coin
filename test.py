a55d35963d94e3bac957ac3b612acbef16db5513
davdcsam <edavidcamposl@gmail.com>
2023-12-10 15:22:50 -0600

    Testing reference system

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
-import os
+import flet as ft
 
-# Obtener la ruta de la carpeta AppData
-appdata_path = os.getenv('APPDATA')
+def btn_click(page, greetings, first_name_arg, last_name_arg):
+    def handler(e):
+        greetings.current.controls.append(
+            ft.Text(f"Hello, {first_name_arg.current.value} {last_name_arg.current.value}!")
+        )
+        first_name_arg.current.value = ""
+        last_name_arg.current.value = ""
+        page.update()
+        first_name_arg.current.focus()
+    return handler
 
-print(appdata_path)
 
-program_files_path = os.getenv('PROGRAMFILES')
+def main(page):
+    first_name = ft.Ref[ft.TextField]()
+    last_name = ft.Ref[ft.TextField]()
+    greetings = ft.Ref[ft.Column]()
 
-print(program_files_path)
+    page.add(
+        ft.TextField(ref=first_name, label="First name", autofocus=True),
+        ft.TextField(ref=last_name, label="Last name"),
+        ft.ElevatedButton("Say hello!", on_click=btn_click(page, greetings, first_name, last_name)),
+        ft.Column(ref=greetings),
+    )
+
+
+ft.app(target=main)
