2ff3391ee32860c236f0c1bb8659d2a1af942d58
davdcsam <edavidcamposl@gmail.com>
2024-03-15 23:45:53 -0600

    Enhance logging and initialize vars in TradeModel
    Refactored the `TradeModel` class to improve clarity and transparency of its operations. Added comments to class and instance variable declarations to explain their purposes. Streamlined the initialization process by explicitly initializing parent classes and setting up instance variables for managing section time, no position status, and logs.
    
    In the `checker` method, refined the logging by explicitly marking entry and exit points and decisions made about input and position checking. The `_operation` method now more clearly differentiates between the logging of successful trade requests and error conditions, ensuring detailed feedback is available for analysis and troubleshooting.
    
    Lastly, added explanatory logging at each significant processing stage, such as initialization, operation, and deinitialization iterations, to aid in monitoring the bot's activity during these phases. This should help in quicker diagnosis of issues and understanding the bot's behavior at any given time. Additionally, the final steps of the operation subtly set flags to denote the shutdown status of the bot, thus ensuring clean exits.
    
    Overall, these changes aim to make the code more maintainable and enhance the operability of the trading bot by providing better insights into its moment-to-moment decision-making processes.

--- model/trade/trade_model.py
+++ model/trade/trade_model.py
@@ -19,6 +19,7 @@ from utils.logs import Logs
 
 
 class TradeModel(Connection, CheckerModule):
+    # Define class variables for order results and requests
     order_result = Any()
     order_result_full_comment = Unicode()
     order_result_request = Any()
@@ -30,8 +31,10 @@ class TradeModel(Connection, CheckerModule):
     order_result_calc_loss = Any()
 
     def __init__(self) -> None:
+        # Initialize parent classes
         Connection.__init__(self)
         CheckerModule.__init__(self)
+        # Initialize instance variables for time section, no position, and logs
         self.instance_section_time = SectionTimeModule()
         self.instance_no_position = NoPositionModule()
         self.instance_logs: Logs = Logs.getInstance()
@@ -43,17 +46,23 @@ class TradeModel(Connection, CheckerModule):
     """
 
     def checker(self, inputs: dict) -> bool:
+        # Initialize order check full comment
         self.order_check_full_comment = ""
 
+        # Format inputs
         formated_inputs: dict[str, Any] = ManagerFiles.get_data_formated(inputs)
 
+        # Check if inputs pass the checker
         if not self._inputs(formated_inputs, self.symbol_info_tick_time):
+            # Log if inputs didn't pass the checker
             self.instance_logs.internal_log(
                 f"Inputs didnt pass checker, {self.order_check_full_comment}", "c"
             )
             return False
+        # Log if inputs pass the checker
         self.instance_logs.log("Inputs pass checker", "c")
 
+        # Check if positions fill mode pass the checker
         if not self._postitions_fill_mode(
             formated_inputs,
             self.symbol_info_name,
@@ -62,12 +71,15 @@ class TradeModel(Connection, CheckerModule):
             self.symbol_info_point,
             self.symbol_info_order_gtc_mode,
         ):
+            # Log if positions didn't pass the checker
             self.instance_logs.internal_log(
                 f"Inputs didnt pass checker, {self.order_check_full_comment}", "c"
             )
             return False
+        # Log if positions pass the checker
         self.instance_logs.log("Positions pass checker", "c")
 
+        # Log if inputs pass the checker
         self.instance_logs.internal_log(
             f"Inputs pass checker, {self.order_check_full_comment}", "c"
         )
@@ -80,6 +92,7 @@ class TradeModel(Connection, CheckerModule):
     """
 
     def _operation(self):
+        # Check if the current time is within a specified section and if there are no positions
         if self.instance_section_time.Any(
             self.symbol_info_tick_time
         ) and self.instance_no_position.Any(
@@ -104,13 +117,14 @@ class TradeModel(Connection, CheckerModule):
                 )
             )._asdict()
 
+            # Store the request details in order_result_request
             self.order_result_request = self.order_result["request"]._asdict()
             for k, v in self.order_result_request.items():
                 if not hasattr(self, f"order_result_request_{k}"):
                     continue
                 self.__setattr__(f"order_result_request_{k}", v)
 
-            # If the trade request was not successful, show an error message
+            # If the trade request was successful, calculate the potential profit and loss
             if self.order_result["retcode"] == mt5.TRADE_RETCODE_DONE:
                 self.order_result_full_comment += "{} order has been placed.".format(
                     self.formated_inputs["input_order_type"],
@@ -131,6 +145,7 @@ class TradeModel(Connection, CheckerModule):
                     self.order_result_request["sl"],
                 )
             else:
+                # If the trade request was not successful, log an error message
                 self.order_result_full_comment = (
                     "{} order has [not] ben placed. Error: {} {}.".format(
                         self.formated_inputs["input_order_type"],
@@ -139,9 +154,12 @@ class TradeModel(Connection, CheckerModule):
                     )
                 )
 
+            # Log the full comment and the order result
             self.instance_logs.log(self.order_result_full_comment, "t")
             self.instance_logs.log(str(self.order_result), "t")
+            # Send a notification that the bot has been shut down
             self.instance_logs.notification("Bot has been shut down.")
+            # Set the deinit_flag and bot_status to indicate that the bot has been shut down
             self.deinit_flag = True
             self.bot_status = False
 
@@ -156,10 +174,12 @@ class TradeModel(Connection, CheckerModule):
         Called when the trading thread is initialized.
         Sets up the trading parameters and verifies the terminal information.
         """
+        # Initialize the section time with the formatted inputs and tick time
         self.instance_section_time.Init(
             self.formated_inputs, self.symbol_info_tick_time
         )
 
+        # Log the initialization iteration, section time, and no positions status
         self.instance_logs.internal_log(
             "InitIteration:{} - Section Time {} - No Positions {}".format(
                 datetime.utcfromtimestamp(
@@ -177,10 +197,12 @@ class TradeModel(Connection, CheckerModule):
         """
         Called during the trading thread.
         Checks the current time and verifies if a trade
-        can be placed based onthe section time.
+        can be placed based on the section time.
         """
+        # Perform the operation
         self._operation()
 
+        # Log the iteration, section time, and no positions status
         self.instance_logs.internal_log(
             "AnyInteration: {} - Section Time {} - No Positions {}".format(
                 datetime.utcfromtimestamp(
@@ -198,7 +220,7 @@ class TradeModel(Connection, CheckerModule):
         """
         This method is called when the trading thread is deinitialized.
         """
-
+        # Log the deinitialization iteration
         self.instance_logs.internal_log(
             "DeinitIteration {}".format(
                 datetime.utcfromtimestamp(
