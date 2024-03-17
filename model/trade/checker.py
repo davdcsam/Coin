7a884145af50a4f0dd80b7003b83551807377e18
davdcsam <edavidcamposl@gmail.com>
2024-03-16 23:45:01 -0600

    Enhanced order validation and logging
    Extended the checker module to solidify the order checking process. A series of validations have been added to ensure the integrity of time inputs, prevent trades outside the specified time section, and affirm sufficient deviation in stop loss parameters, thereby mitigating potential trade errors. Also, order checks now iterate through various filling modes to set the real filling mode accurately, and detailed internal logging has been implemented to track both successful and failed order checks. Additionally, the module now calculates potential profit and loss upon a successful check, while it resets these values when an order check fails, aiding in risk management.

--- model/trade/checker.py
+++ model/trade/checker.py
@@ -14,6 +14,7 @@ from utils.logs import Logs
 
 
 class CheckerModule(HasTraits):
+    # Define class variables for order checks
     order_check = Any()
     order_check_retcode = Any()
     order_check_full_comment = Unicode()
@@ -26,14 +27,17 @@ class CheckerModule(HasTraits):
     order_check_calc_loss = Any()
 
     def __init__(self) -> None:
+        # Initialize instance variables for section time and logs
         self.instance_section_time = SectionTimeModule()
         self.instance_logs: Logs = Logs.getInstance()
 
     def _inputs(self, formated_inputs: dict, symbol_info_tick_time: int) -> bool:
+        # Verify the existence of start and end times from the input
         start_time, end_time = self.instance_section_time.verify_existence_from_input(
             formated_inputs, symbol_info_tick_time
         )
 
+        # Check if start time equals end time
         if start_time == end_time:
             self.order_check_full_comment += (
                 "Start Time {} cannot equals to End Time {}.\n".format(
@@ -42,6 +46,7 @@ class CheckerModule(HasTraits):
             )
             return False
 
+        # Check if the broker's current time is after the set period
         if datetime.utcfromtimestamp(symbol_info_tick_time) >= end_time:
             self.order_check_full_comment += "The broker's current time {} is after the set period {} to {}. The currency can send trades within the time section.\n".format(
                 datetime.utcfromtimestamp(symbol_info_tick_time).strftime("%H:%M:%S"),
@@ -50,6 +55,7 @@ class CheckerModule(HasTraits):
             )
             return False
 
+        # Check if the deviation may not be sufficient
         if (
             formated_inputs["input_stop_loss"] * 0.01
             > formated_inputs["input_deviation_trade"]
@@ -68,12 +74,14 @@ class CheckerModule(HasTraits):
         point: Any,
         time_mode: Any = mt5.ORDER_TIME_GTC,
     ) -> bool:
+        # Iterate over different filling modes
         for filling_mode in [
             mt5.ORDER_FILLING_FOK,
             mt5.ORDER_FILLING_IOC,
             mt5.ORDER_FILLING_RETURN,
             mt5.ORDER_FILLING_BOC,
         ]:
+            # Build the request
             request = RequestModule.build(
                 symbol,
                 formated_inputs["input_lot_size"],
@@ -88,8 +96,10 @@ class CheckerModule(HasTraits):
                 filling_mode,
             )
 
+            # Check the order
             order_check = mt5.order_check(request)
 
+            # If the order check fails, log an error and return False
             if not order_check:
                 self.instance_logs.notification(
                     "Error when indexing request format to check order. Contact to bot developer.",
@@ -97,13 +107,16 @@ class CheckerModule(HasTraits):
                 )
                 return False
 
+            # If the retcode is not invalid fill, set the real filling mode and break the loop
             if order_check.retcode != mt5.TRADE_RETCODE_INVALID_FILL:
                 self.symbol_info_filling_mode_real = order_check.request.type_filling
                 break
 
+        # Convert the order check to a dictionary and log it
         self.order_check = order_check._asdict()
         self.instance_logs.internal_log(str(self.order_check), "c")
 
+        # Set the full comment based on the order check comment
         order_check_comment_temp = self.order_check["comment"]
         if order_check_comment_temp == "Done":
             self.order_check_full_comment += "{} order can be placed.\n".format(
@@ -113,12 +126,14 @@ class CheckerModule(HasTraits):
             self.order_check_full_comment += "{}.\n".format(self.order_check["comment"])
         self.order_check_retcode = self.order_check["retcode"]
 
+        # Convert the order check request to a dictionary and set the corresponding class variables
         self.order_check_request = self.order_check["request"]._asdict()
         for k, v in self.order_check_request.items():
             if not hasattr(self, f"order_check_request_{k}"):
                 continue
             self.__setattr__(f"order_check_request_{k}", v)
 
+        # If the retcode is 0, calculate the potential profit and loss and return True
         if order_check.retcode == 0:
             self.order_check_calc_profit = mt5.order_calc_profit(
                 self.order_types_dict[formated_inputs["input_order_type"]],
@@ -136,6 +151,7 @@ class CheckerModule(HasTraits):
             )
             return True
         else:
+            # If the retcode is not 0, set the potential profit and loss to 0 and return False
             self.order_check_calc_profit = 0
             self.order_check_calc_loss = 0
             return False
