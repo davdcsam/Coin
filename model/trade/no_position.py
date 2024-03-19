9838174589e992bf0a5c54b78e67de874f25e6b5
davdcsam <edavidcamposl@gmail.com>
2024-03-18 19:29:55 -0600

    Enhance position verification logic
    Added comments to clarify the steps in the NoPositionModule's methods, ensuring a clear understanding of the code's intention. Improved readability by condensing the condition checking for positions with a magic number of 0. These changes make the logic for verifying the absence of positions more transparent and maintainable.

--- model/trade/no_position.py
+++ model/trade/no_position.py
@@ -8,24 +8,28 @@ import pandas as pd
 
 class NoPositionModule:
     def __init__(self) -> None:
+        # Initialize the module
         pass
 
     def Init(self, inputs: dict, positions_symbol: pd.DataFrame):
+        # Verify if there are no positions for the given inputs and symbol positions
         return self._verify_no_positions(inputs, positions_symbol)
 
     def Any(self, inputs: dict, positions_symbol: pd.DataFrame):
+        # Verify if there are no positions for the given inputs and symbol positions
         return self._verify_no_positions(inputs, positions_symbol)
 
     def _verify_no_positions(
         self, inputs: dict, positions_symbol: pd.DataFrame
     ) -> bool:
+        # If there are no positions for the symbol, return True
         if positions_symbol.empty:
             return True
 
-        positions_symbol_magic = positions_symbol[
-            positions_symbol["magic"] == 0
-        ]
+        # Filter the positions where magic number is 0
+        positions_symbol_magic = positions_symbol[positions_symbol["magic"] == 0]
 
+        # If there are no positions with magic number 0, return True. Otherwise, return False
         if positions_symbol_magic.empty:
             return True
         else:
